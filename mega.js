ead065d6221f842e97d38c5deeb13d367a948b9b
Akewushola abdulbakri temitope <neaterry6@gmail.com>
2025-04-06 02:32:12 +0100

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ const mega = require("megajs");
 
 const auth = {
     email: 'akewusholaabdulbakri101@gmail.com',   //use your real vaild mega account email
-    password: '5ZNx-Qcc_bHaPhb',  ////use your real vaild mega account password
+    password: '5ZNx-Qcc_bHaPHb',  ////use your real vaild mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
